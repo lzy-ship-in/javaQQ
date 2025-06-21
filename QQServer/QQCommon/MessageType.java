24097de9816f4b37f18880fdb08e330275d288ac
lzy-ship-in <1570593393@qq.com>
2025-06-21 11:47:28 +0800

    Add files via upload

---
+++ QQServer/QQCommon/MessageType.java
@@ -0,0 +1,13 @@
+package com.hspedu.qqcommon;
+
+public interface MessageType {
+	   String MESSAGE_LOGIN_SUCCEED="1";//表示登录成功
+	   String MESSAGE_LOGIN_FAIT="2";//表示登录失败
+	   String MESSAGE_COMM_MES="3";//普通信息包
+	   String MESSAGE_GET_ONLINE_FRIEND="4";//要求返回在线用户列表
+	   String MESSAGE_RET_ONLINE_FRIEND="5";//返回在线用户列表
+	   String MESSAGE_CLIENT_EXIT="6";//客户端请求退出
+	   String MESSAGE_TO_ALL_MES = "7"; // 群发消息
+	    String MESSAGE_FILE_MES = "8";   // 文件消息
+}
+
